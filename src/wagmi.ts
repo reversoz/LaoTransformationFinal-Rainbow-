f33402ac3ee5ec253f399f5847957332bed2f781
EliteGoodDev <michaeljohnares.1026@gmail.com>
2025-06-10 08:58:25 -0500

    change into mainnet

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,11 +1,11 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { mainnet, sepolia } from 'wagmi/chains';
+import { mainnet } from 'wagmi/chains';
 
 // Get your project ID at https://cloud.walletconnect.com
 // This is required for WalletConnect v2
 export const config = getDefaultConfig({
   appName: 'RainbowKit App',
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || 'YOUR_PROJECT_ID',
-  chains: [sepolia],
+  chains: [mainnet],
   ssr: true,
 });
