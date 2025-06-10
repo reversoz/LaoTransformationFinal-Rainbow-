700afec5982dd229f427c814d113fe665c73496a
EliteGoodDev <michaeljohnares.1026@gmail.com>
2025-06-10 18:21:02 -0500

    change favicon

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="icon" href="/ozLogo.svg" type="image/svg+xml" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+} 
