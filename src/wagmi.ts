bd88e824bacb6d427633bbb4e5a8071204691fcd
ozlabz <info@ozlabs.io>
2026-04-13 07:00:09 +0000

    fix: use alchemy RPC instead of public merkle fallback

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,11 +1,13 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { mainnet } from 'wagmi/chains';
+import { http } from 'wagmi';
 
-// Get your project ID at https://cloud.walletconnect.com
-// This is required for WalletConnect v2
 export const config = getDefaultConfig({
   appName: 'laotransformation',
   projectId: 'd6506f86d5777c258b2c9cbc522dc704',
   chains: [mainnet],
+  transports: {
+    [mainnet.id]: http('https://eth-mainnet.g.alchemy.com/v2/Lhl4ddnfjJecFpB_6rUsx'),
+  },
   ssr: true,
 });
